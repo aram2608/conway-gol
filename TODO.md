9e9ddb989d39b07e01f4c870fb1086153209a7ae
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-09-26 13:11:04 -0400

    window drawing

---
+++ TODO.md
@@ -0,0 +1,8 @@
+# TODO: Conway's Game of Life
+
+- [x] Create a Blank Screen and Simulation Loop
+- [] Create the Grid
+- [] Count live neighbors
+- [] Implement simulation rules
+- [] Starting and stopping the simulation
+- [] Create custom initial states
--- cgol/src/main.cpp
+++ cgol/src/main.cpp
@@ -1,30 +1,28 @@
-#include <iostream>
 #include <raylib.h>
 
 int main() {
-    // Hard coded window values w/offest
-    int off_set = 50;
-    int window_w = 750;
-    int window_h = 700;
+    // Hard coded window values
+    const int window_w = 750;
+    const int window_h = 750;
+    Color dark_green = Color{20, 160, 133, 255};
 
-    // Game window Audio Device //
+    // Game window //
     // INITIALIZE FIRST BEFORE LOADING GPU RESOURCES //
-    InitWindow(window_w + off_set, window_h + (2 * off_set),
-               "C++ - Game of Life");
+    InitWindow(window_w, window_h, "C++ - Game of Life");
 
-    // Target FPS of 60
-    SetTargetFPS(60);
+    // Target FPS of 12
+    SetTargetFPS(12);
 
-    // Game loop. Runs will the window is not closed
+    // Simulation loop. Runs will the window is not closed
     while (!WindowShouldClose()) {
 
         // Main logic for drawing to game window
         BeginDrawing();
+        ClearBackground(dark_green);
 
         EndDrawing();
     }
     // Close our game window
     CloseWindow();
-    CloseAudioDevice();
     return 0;
 }
